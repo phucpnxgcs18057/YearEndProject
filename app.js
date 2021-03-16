c8cc9f97a7ebc3e90d14b25b4868bddc6c924057
phucpnxgcs18057 <isfunz1@gmail.com>
2021-03-16 23:09:25 +0700

    Turned html to ejs
    I have turn html pages in to ejs pages, and create partials ejs pages for easier adjustment.

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+//express app
+const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use("/public", express.static('public'))
+
+//listen for requests
+app.listen(3000);
+
+//routing
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/courses-single', (req, res) => {
+    res.render('course-single');
+});
+
+app.get('/forget', (req, res) => {
+    res.render('forget');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/resources', (req, res) => {
+    res.render('resources');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+//404 page
+app.use((req, res) => {
+    res.status(404).render('404');
+});
